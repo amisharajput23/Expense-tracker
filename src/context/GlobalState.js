18516b807e3f698f974f2450d041614ca3ad5205
amisha <rajputamisha34@gmail.com>
2021-04-17 17:54:03 +0530

    completed

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -2,13 +2,8 @@ import React, {createContext, useReducer} from 'react';
 import AppReducer from './AppReducer';
 
 const initialState = {
-    transactions: 
-     [
-           { id: 1, text: 'Flower', amount: -20 },
-           { id: 2, text: 'Salary', amount: 300 },
-           { id: 3, text: 'Book', amount: -10 },
-           { id: 4, text: 'Camera', amount: 150 }
-        ]
+    transactions:[]
+        
         
 }
 
@@ -26,9 +21,19 @@ export const  GlobalProvider = ({ children }) => {
             payload: id,
     });
 }
+
+function addTransaction(transaction){
+    dispatch({
+        type: 'ADD_TRANSACTION',
+        payload: transaction
+});
+}
+
+
     return (< GlobalContext.Provider value = {{
         transactions:state.transactions,
         deleteTransaction,
+        addTransaction,
     }}>
         {children}
     
